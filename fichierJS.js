8f43adf555f4718211ede9c36b6cdc83bdff9caa
steelanterns <steevejeanchilles@gmail.com>
2019-05-27 16:51:52 -0500

    derniere modif du fichier js,ajout d'un autre message de remerciment

--- fichierJS.js
+++ fichierJS.js
@@ -2,4 +2,5 @@ let message = "Hello word";
 
 let message1 = "Bonjour tout le monde";
 
-let message2 = "Merci a mes corecteurs"
+let message2 = "Merci a mes corecteurs";
+let message3 = "Merci au prof \"Marc\"";
